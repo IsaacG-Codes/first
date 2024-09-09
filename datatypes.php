0e11378dd8120eeb799ddf3bbc24d76aefaaf082
IsaacG-Codes <Isaac.Gao@students.jmcss.org>
2024-09-09 07:43:34 -0500

    Create datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    //String
+    $name = 'Coding is useful';
+
+    //integer
+    $name = 30;
+
+    //Float
+    $name = 20.209;
+
+    //Boolean
+    // true = 1
+    // false = 0
+    //Array 
+    $names = array("Math","English","Science");
+    echo $names['1'];
+
+?>
+
+</body>
+</html>
