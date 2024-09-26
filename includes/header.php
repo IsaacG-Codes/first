d27ba1832b5d8041ddc4f17a24797fc9081e0bb9
IsaacG-Codes <Isaac.Gao@students.jmcss.org>
2024-09-26 08:42:23 -0500

    loginsystem day 1
    18:56 on video

--- includes/header.php
+++ includes/header.php
@@ -1,66 +1,35 @@
-<?php
-session_start();
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <title>Document</title>
     <link rel="stylesheet" href="style.css">
   </head>
-  <body>
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-        
-</head>
 <body>
-<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Home Page</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        <a class="nav-link" href="arrays.php">arrays</a>
-        <a class="nav-link" href="calculator.php">calculator</a>
-        <a class="nav-link" href="calendar.php">calendar</a>
-        <a class="nav-link" href="conditionals.php">conditionals</a>
-        <a class="nav-link" href="datatypes.php">datatypes</a>
-        <a class="nav-link" href="functions.php">functions</a>
-        <a class="nav-link" href="loops.php">loops</a>
-        <a class="nav-link" href="operators.php">operators</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+    <header>
+      <nav class="nav-header-main">
+        <a class="header-logo" href="index.php">
+          <img src="img/logo.png" alt="logo">
+    </a>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">About me</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+      <div class="header-login">
+        <form action="includes/login.inc.php" method="POST">
+        <input type="text" name="mailuid" placeholder="Username/E-mail...">
+        <input type="password" name="pwd" placeholder="Password...">
+        <button type="submit" name="login-submit">Login</button>
+
+      </form>
+      <a href="signup.php">Signup</a>
+      <form action="includes/login.inc.php" method="POST">
+        <button type="submit" name="logout-submit">Logout</button>
       </div>
-    </div>
-  </div>
-</nav>
-
-<?php
-
-$_SESSION['username'] = "Isaac1234G";
-echo $_SESSION['username'];
-
-if(!isset($_SESSION['username'])){
-echo "You are not logged in!";
-
-}
-else {
-  echo "You are logged in.";
-}
-?>
-
+    </nav>
+  </header>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,39 +1,12 @@
-<?php
-    include 'includes/header.php';
+<?php 
+  require "includes/header.php";
 ?>
 
-<!doctype html>
-<html lang="eng">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
-
-    <title>Hello world!</title>
-  </head>
-  <body>
-    <h1>Hello world!</h1>
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <?php
-    //This prints something on the screen
-    /*
-    echo "My first website";*/
-    ?>
-
-<form method="GET">
-    <input type= "text" name="edible">
-    <button>SUBMIT</button>
-</form>
-<?php
-    $name = $_GET['edible'];
-    echo $name." is totally edible!!!";
-?>
-
-  </body>
-</html>
+  <main>
+    <div class="wrapper-main">
+      <section class="section-default">
+        <p class="login-status">You are logged out!</p>
+        <p class="login-status">You are logged in!</p>
+      </section>
+    </div>
+  </main>
